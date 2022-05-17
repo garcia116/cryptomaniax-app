90c116102532bfe0c1ef9875a8639a7f77dd4a61
garcia116 <38366630+garcia116@users.noreply.github.com>
2022-05-16 20:21:31 -0700

    fix cors error

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // Note: Change v1 to v2 on rapid api
 
 const cryptoApiHeaders = {
-  'Access-Control-Allow-Origin': "https://garcia116.github.io/cryptomaniax-app/",
+  'Access-Control-Allow-Origin': "*",
   'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
   'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
 };
